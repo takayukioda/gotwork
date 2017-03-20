b3fcd4a0c6f2515ac2f79e2d47a23bee27c739c5
takayukioda <takayuki@odashi.me>
2017-03-21 02:37:27 +0900

    feat: get room api

--- chatwork.go
+++ chatwork.go
@@ -16,19 +16,19 @@ type Chatwork struct {
 }
 
 type Room struct {
-	RoomId         int    `json:room_id`
-	Name           string `json:name`
-	Type           string `json:type`
-	Role           string `json:role`
-	Sticky         bool   `json:sticky`
-	UnreadNum      int    `json:unread_num`
-	MentionNum     int    `json:mention_num`
-	MytaskNum      int    `json:mytask_num`
-	MessageNum     int    `json:message_num`
-	FileNum        int    `json:file_num`
-	TaskNum        int    `json:task_num`
-	IconPath       string `json:icon_path`
-	LastUpdateTime string `json:last_update_time`
+	RoomId         int    `json:"room_id"`
+	Name           string `json:"name"`
+	Type           string `json:"type"`
+	Role           string `json:"role"`
+	Sticky         bool   `json:"sticky"`
+	UnreadNum      int    `json:"unread_num"`
+	MentionNum     int    `json:"mention_num"`
+	MytaskNum      int    `json:"mytask_num"`
+	MessageNum     int    `json:"message_num"`
+	FileNum        int    `json:"file_num"`
+	TaskNum        int    `json:"task_num"`
+	IconPath       string `json:"icon_path"`
+	LastUpdateTime int    `json:"last_update_time"`
 }
 
 func NewChatwork(token string) *Chatwork {
@@ -51,32 +51,34 @@ func (c *Chatwork) endpoint(path string) string {
 	return fmt.Sprintf("%s%s", rooturl.String(), path)
 }
 
+func (c *Chatwork) request(method string, path string) (*http.Response, error) {
+	req, err := http.NewRequest(method, c.endpoint(path), nil)
+	if err != nil {
+		println("Failed on creating new request")
+		log.Fatal(err)
+	}
+	req.Header.Add("User-Agent", "chatwork-client by Go")
+	req.Header.Add("X-ChatWorkToken", c.token)
+
+	return c.client.Do(req)
+}
+
 func (c *Chatwork) GetRooms() []Room {
 	const (
 		method = "GET"
 		path   = "/rooms"
 	)
-	req, e := http.NewRequest(method, c.endpoint(path), nil)
-	if e != nil {
-		println("Failed on creating new request")
-		log.Fatal(e)
-	}
-	req.Header.Add("User-Agent", "chatwork-client by Go")
-	req.Header.Add("X-ChatWorkToken", c.token)
-
-	resp, er := c.client.Do(req)
-	if er != nil {
+	resp, err := c.request(method, path)
+	if err != nil {
 		println("Failed on api call")
-		log.Fatal(er)
+		log.Fatal(err)
 	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
 
 	var rooms []Room
-	err := json.Unmarshal(buf.Bytes(), &rooms)
+	err = json.NewDecoder(resp.Body).Decode(&rooms)
 	if err != nil {
-		println("Failed on unmarshal the result")
-		log.Fatal(er)
+		println("Failed on decode the result")
+		log.Fatal(err)
 	}
 	return rooms
 }
--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 func main() {
 	var chatwork *Chatwork = NewChatwork(os.Getenv("CHATWORK_TOKEN"))
 	var rooms []Room = chatwork.GetRooms()
+	var members []Room
 	for _, room := range rooms {
-		fmt.Printf("%#v\n", room)
+		switch room.Role {
+		case "member":
+			members = append(members, room)
+		default:
+			println("Other than member", room.Role, room.Name)
+		}
 	}
+	fmt.Printf("Members %v\n", members)
 }
